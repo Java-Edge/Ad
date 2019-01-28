1d36eaf20a1c6c41f5d184357c3631ee629a0279
ShiShusheng <2855845850@qq.com>
2019-01-28 11:12:25 +0800

    单节点 eureka-sever 配置与部署

---
+++ ad-eureka/src/main/java/com/javaedge/ad/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.javaedge.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author sss
+ * @date 2019/1/28
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
