7002ee688421dede463e82c60218c793b064b69f
ShiShusheng <2855845850@qq.com>
2019-01-31 16:09:54 +0800

    统一配置的开发

---
+++ javaedge-ad-service/ad-common/src/main/java/com/javaedge/ad/conf/WebConfiguration.java
@@ -0,0 +1,22 @@
+package com.javaedge.ad.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * @author sss
+ * @date 2019/1/31
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.clear();
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+}
