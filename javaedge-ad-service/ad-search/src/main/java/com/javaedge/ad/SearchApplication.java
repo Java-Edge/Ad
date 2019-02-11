f1a982bca1d0a3da2322ef4770d8138ce68c22cf
ShiShusheng <2855845850@qq.com>
2019-02-11 10:47:34 +0800

    创建广告检索系统子模块

---
+++ javaedge-ad-service/ad-search/src/main/java/com/javaedge/ad/SearchApplication.java
@@ -0,0 +1,40 @@
+package com.javaedge.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author sss
+ * @date 2019-02-11
+ */
+@EnableFeignClients
+@EnableEurekaClient
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@SpringBootApplication
+public class SearchApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SearchApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+
+        return new RestTemplate();
+    }
+}
+
