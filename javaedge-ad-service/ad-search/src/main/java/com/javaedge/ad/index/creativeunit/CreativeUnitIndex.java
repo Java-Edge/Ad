aec66aa94f566ce869a60516193972facfdd3538
ShiShusheng <2855845850@qq.com>
2019-02-12 10:22:08 +0800

    创意与推广单元关联索引对象定义与服务实现

---
+++ javaedge-ad-service/ad-search/src/main/java/com/javaedge/ad/index/creativeunit/CreativeUnitIndex.java
@@ -0,0 +1,120 @@
+package com.javaedge.ad.index.creativeunit;
+
+import com.javaedge.ad.index.IndexAware;
+import com.javaedge.ad.index.adunit.AdUnitObject;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+/**
+ * 创意与推广单元关联索引
+ *
+ * @author sss
+ * @date 2019-02-12
+ */
+@Slf4j
+@Component
+public class CreativeUnitIndex implements IndexAware<String, CreativeUnitObject> {
+
+    /**
+     * <adId-unitId, CreativeUnitObject>
+     */
+    private static Map<String, CreativeUnitObject> objectMap;
+    /**
+     * <adId, unitId Set>
+     */
+    private static Map<Long, Set<Long>> creativeUnitMap;
+    /**
+     * <unitId, adId set>
+     */
+    private static Map<Long, Set<Long>> unitCreativeMap;
+
+    static {
+        objectMap = new ConcurrentHashMap<>();
+        creativeUnitMap = new ConcurrentHashMap<>();
+        unitCreativeMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public CreativeUnitObject get(String key) {
+        return objectMap.get(key);
+    }
+
+    @Override
+    public void add(String key, CreativeUnitObject value) {
+
+        log.info("before add: {}", objectMap);
+
+        objectMap.put(key, value);
+
+        Set<Long> unitSet = creativeUnitMap.get(value.getAdId());
+        if (CollectionUtils.isEmpty(unitSet)) {
+            unitSet = new ConcurrentSkipListSet<>();
+            creativeUnitMap.put(value.getAdId(), unitSet);
+        }
+        unitSet.add(value.getUnitId());
+
+        Set<Long> creativeSet = unitCreativeMap.get(value.getUnitId());
+        if (CollectionUtils.isEmpty(creativeSet)) {
+            creativeSet = new ConcurrentSkipListSet<>();
+            unitCreativeMap.put(value.getUnitId(), creativeSet);
+        }
+        creativeSet.add(value.getAdId());
+
+        log.info("after add: {}", objectMap);
+    }
+
+    @Override
+    public void update(String key, CreativeUnitObject value) {
+
+        log.error("CreativeUnitIndex not support update");
+    }
+
+    @Override
+    public void delete(String key, CreativeUnitObject value) {
+
+        log.info("before delete: {}", objectMap);
+
+        objectMap.remove(key);
+
+        Set<Long> unitSet = creativeUnitMap.get(value.getAdId());
+        if (CollectionUtils.isNotEmpty(unitSet)) {
+            unitSet.remove(value.getUnitId());
+        }
+
+        Set<Long> creativeSet = unitCreativeMap.get(value.getUnitId());
+        if (CollectionUtils.isNotEmpty(creativeSet)) {
+            creativeSet.remove(value.getAdId());
+        }
+
+        log.info("after delete: {}", objectMap);
+    }
+
+    public List<Long> selectAds(List<AdUnitObject> unitObjects) {
+
+        if (CollectionUtils.isEmpty(unitObjects)) {
+            return Collections.emptyList();
+        }
+
+        List<Long> result = new ArrayList<>();
+
+        for (AdUnitObject unitObject : unitObjects) {
+
+            Set<Long> adIds = unitCreativeMap.get(unitObject.getUnitId());
+            if (CollectionUtils.isNotEmpty(adIds)) {
+                result.addAll(adIds);
+            }
+        }
+
+        return result;
+    }
+}
+
---
+++ javaedge-ad-service/ad-search/src/main/java/com/javaedge/ad/index/creativeunit/CreativeUnitObject.java
@@ -0,0 +1,20 @@
+package com.javaedge.ad.index.creativeunit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author sss
+ * @date 2019-02-12
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreativeUnitObject {
+
+    private Long adId;
+    private Long unitId;
+
+    // adId-unitId
+}
