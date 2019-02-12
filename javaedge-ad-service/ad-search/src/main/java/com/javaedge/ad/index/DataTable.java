5266ff1e129fe91152b403e1bb6c18bb13ef01e1
ShiShusheng <2855845850@qq.com>
2019-02-12 10:42:41 +0800

    索引服务类缓存的实现

---
+++ javaedge-ad-service/ad-search/src/main/java/com/javaedge/ad/index/DataTable.java
@@ -0,0 +1,54 @@
+package com.javaedge.ad.index;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.PriorityOrdered;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author sss
+ * @date 2019-02-12
+ */
+@Component
+public class DataTable implements ApplicationContextAware, PriorityOrdered {
+
+    private static ApplicationContext applicationContext;
+
+    public static final Map<Class, Object> dataTableMap = new ConcurrentHashMap<>();
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        DataTable.applicationContext = applicationContext;
+    }
+
+    @Override
+    public int getOrder() {
+        return PriorityOrdered.HIGHEST_PRECEDENCE;
+    }
+
+    @SuppressWarnings("all")
+    public static <T> T of(Class<T> clazz) {
+
+        T instance = (T) dataTableMap.get(clazz);
+        if (null != instance) {
+            return instance;
+        }
+
+        dataTableMap.put(clazz, bean(clazz));
+        return (T) dataTableMap.get(clazz);
+    }
+
+    @SuppressWarnings("all")
+    private static <T> T bean(String beanName) {
+        return (T) applicationContext.getBean(beanName);
+    }
+
+    @SuppressWarnings("all")
+    private static <T> T bean(Class clazz) {
+        return (T) applicationContext.getBean(clazz);
+    }
+}
