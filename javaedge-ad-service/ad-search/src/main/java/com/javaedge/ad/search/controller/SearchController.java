d44ae32e79d236d396f3e44f4f5c7c8ddfd46b4b
ShiShusheng <2855845850@qq.com>
2019-02-27 11:17:50 +0800

    完善广告检索服务

---
+++ javaedge-ad-service/ad-search/src/main/java/com/javaedge/ad/search/controller/SearchController.java
@@ -0,0 +1,75 @@
+package com.javaedge.ad.search.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.javaedge.ad.annotation.IgnoreResponseAdvice;
+import com.javaedge.ad.client.SponsorClient;
+import com.javaedge.ad.client.vo.AdPlan;
+import com.javaedge.ad.client.vo.AdPlanGetRequest;
+import com.javaedge.ad.search.ISearch;
+import com.javaedge.ad.search.vo.SearchRequest;
+import com.javaedge.ad.search.vo.SearchResponse;
+import com.javaedge.ad.vo.CommonResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * @author sss
+ * @date 2019-02-26
+ */
+@Slf4j
+@RestController
+public class SearchController {
+
+    private final ISearch search;
+
+    private final RestTemplate restTemplate;
+
+    private final SponsorClient sponsorClient;
+
+    @Autowired
+    public SearchController(RestTemplate restTemplate,
+                            SponsorClient sponsorClient, ISearch search) {
+        this.restTemplate = restTemplate;
+        this.sponsorClient = sponsorClient;
+        this.search = search;
+    }
+
+    @PostMapping("/fetchAds")
+    public SearchResponse fetchAds(@RequestBody SearchRequest request) {
+
+        log.info("ad-search: fetchAds -> {}",
+                JSON.toJSONString(request));
+        return search.fetchAds(request);
+    }
+
+    @IgnoreResponseAdvice
+    @PostMapping("/getAdPlans")
+    public CommonResponse<List<AdPlan>> getAdPlans(
+            @RequestBody AdPlanGetRequest request
+    ) {
+        log.info("ad-search: getAdPlans -> {}",
+                JSON.toJSONString(request));
+        return sponsorClient.getAdPlans(request);
+    }
+
+    @SuppressWarnings("all")
+    @IgnoreResponseAdvice
+    @PostMapping("/getAdPlansByRibbon")
+    public CommonResponse<List<AdPlan>> getAdPlansByRebbon(
+            @RequestBody AdPlanGetRequest request
+    ) {
+        log.info("ad-search: getAdPlansByRibbon -> {}",
+                JSON.toJSONString(request));
+        return restTemplate.postForEntity(
+                "http://eureka-client-ad-sponsor/ad-sponsor/get/adPlan",
+                request,
+                CommonResponse.class
+        ).getBody();
+    }
+}
